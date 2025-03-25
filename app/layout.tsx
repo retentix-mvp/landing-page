5caec429c2901400570f04ed1992b8d0ecec4561
Joaquim Alves <jafn92@gmail.com>
2025-03-24 22:45:05 -0300

    123...

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '"Retentix | IA para Corretores de Imóveis Automatizarem Atendimento e Triplicarem Vendas',
+  title: "Retentix | IA para Corretores de Imóveis",
   description: 'Assistente virtual com IA para corretores e imobiliárias que atende, qualifica e converte leads 24/7. Aumente suas vendas em até 300% sem contratar assistentes.',
 }
 
